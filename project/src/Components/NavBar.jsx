d4e18d487a95f18806b11b777290ea12372126c6
dyeem <johnmarknavajas14@gmail.com>
2024-11-13 10:04:03 +0800

    13/11 10:03am

--- project/src/Components/NavBar.jsx
+++ project/src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ import logo from '../Assets/logo.png'
 
 export default function NavBar({NavLink}) {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-300">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">

46c86e5f4ca0008ec1ed5f38123f725770be0937
dyeem <johnmarknavajas14@gmail.com>
2024-12-18 16:08:00 +0800

    4:07pm handle error pages

--- project/src/Layouts/router.jsx
+++ project/src/Layouts/router.jsx
@@ -15,7 +15,6 @@ import Sales from "../Components/Sales.jsx";
 import OurTeam from "../Components/OurTeam.jsx";
 import FaqPage from "../Components/FaqPage.jsx";
 import OCPage from "../Components/OCPage.jsx";
-import Cart from "../Components/Cart.jsx";
 
 
 //Root Layout
@@ -23,6 +22,9 @@ import RootLayout from '../Layouts/RootLayout.jsx'
 import SalesLayout from "./SalesLayout.jsx";
 import CheckoutLayout from "./CheckoutLayout.jsx";
 import HelpLayout from "./HelpLayout.jsx";
+
+//error page
+import ErrorPage from '../Components/ErrorPage/PagenotFound.jsx'
 
 
 const router = createBrowserRouter(createRoutesFromElements(
@@ -32,22 +34,27 @@ const router = createBrowserRouter(createRoutesFromElements(
       <Route index element={<MainContent />} />
       <Route path="aboutus" element={<AboutUs />} />
       <Route path="reviews" element={<Reviews />} />
-
-      {/* <Route path="sales" element={<Sales />} loader={productsLoader} /> */}
       <Route path="products" element={<SalesLayout />}>
         <Route index element={<Sales />} />
         <Route path="checkout" element={<CheckoutLayout />}>
           <Route index element={<CheckOut />} />
           <Route path="orderconfirmationpage" element={<OCPage />} />
+          {/* Handle missing/mismatched page */}
+          <Route path="*" element={<ErrorPage/>}/> 
         </Route>
+        {/* Handle missing/mismatched page */}
+        <Route path="*" element={<ErrorPage/>}/> 
       </Route>
-
       <Route path="help" element={<HelpLayout/>}>
         <Route path="contactus" element={<ContactUs />} />
         <Route path="faqpage" element={<FaqPage />}/>
+        {/* Handle missing/mismatched page */}
+        <Route path="*" element={<ErrorPage/>}/> 
       </Route>
-
       <Route path="ourteam" element={<OurTeam />} />
+      
+      {/* Handle missing/mismatched page */}
+      <Route path="*" element={<ErrorPage/>}/> 
     </Route>
   </>
 ))
